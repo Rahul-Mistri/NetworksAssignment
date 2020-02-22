3cf0d5f520221fdd3dfcdbeeaefe081b983fcdca
georgeo <georgeosabu@gmail.com>
2020-02-22 18:53:37 +0200

    contnious query unless quit entered basics

--- NetworkingAss/src/app/MyClient.java
+++ NetworkingAss/src/app/MyClient.java
@@ -3,6 +3,7 @@ import java.io.*;
 import java.net.*;  
 import java.util.*;
 
+
 public class MyClient {  
     private static final int SERVERPORT=6666;
     private static final String SERVERIP="localhost";
@@ -10,9 +11,18 @@ public class MyClient {
         try{      
         Socket s=new Socket(SERVERIP,SERVERPORT);  
         BufferedReader in=new BufferedReader(new InputStreamReader(s.getInputStream()));
+        BufferedReader fromUser=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(s.getOutputStream(),true);
+
+        while(true){
+            String userIn=fromUser.readLine();
+            if(userIn.equals("quit")){break;}
+            out.println(userIn);
+
             String serverResponse=in.readLine();
 
             System.out.println(serverResponse);
+        }
         s.close();  
         }
         catch(Exception e){System.out.println(e);}  
--- NetworkingAss/src/app/MyServer.java
+++ NetworkingAss/src/app/MyServer.java
@@ -5,16 +5,30 @@ import java.net.*;
 import java.util.*;
 public class MyServer {  
     private static final int PORT=6666;
+    private static String[] names={"Georgeo","Rahul","Silas"};
     public static void main(String[] args){  
         try{  
             ServerSocket ss=new ServerSocket(PORT);  
             Socket s=ss.accept();//establishes connection   
             PrintWriter out=new PrintWriter(s.getOutputStream(),true);
-            String date=(new Date()).toString();
-            out.println(date);
+            BufferedReader in=new BufferedReader(new InputStreamReader(s.getInputStream()));
+            while(true){
+                String request=in.readLine();
+                if (request.contains("name")){
+                    out.println(getRandom());
+                }
+                else{
+                    out.println("Error");
+                    break;
+                }
+            }
             s.close();
             ss.close();  
         }
         catch(Exception e){System.out.println(e);}  
     }  
+    public static String getRandom(){
+        String name =names[(int)(Math.random()*names.length)];
+        return name;
+    }
 }  

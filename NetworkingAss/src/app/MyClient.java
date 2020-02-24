1a4eb49948dd24134e8a43df5854e67712ef33af
georgeo <georgeosabu@gmail.com>
2020-02-24 23:48:23 +0200

    multithreaded clients communicate via unique ports

--- NetworkingAss/src/app/MyClient.java
+++ NetworkingAss/src/app/MyClient.java
@@ -5,28 +5,55 @@ import java.net.*;
 import java.util.*;
 
 public class MyClient {
+
+    // Setting the constant connection port
     private static final int SERVERPORT = 6666;
+    // setting the constant ip of the server
     private static final String SERVERIP = "localhost";
 
     public static void main(String[] args) {
         try {
-            Socket s = new Socket(SERVERIP, SERVERPORT);
-            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            // connectionSocket is making a connection request to the server using the
+            // serversIp and the port 6666.
+            Socket connectionSocket = new Socket(SERVERIP, SERVERPORT);
+            // bufferedReader connectionIn is used to receive a unique PORT number from the
+            // server such that the server and the client can talk through this port.
+            BufferedReader connectionIn = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            // serverResponseWithPortNumber takes in the unique communication port number as
+            // a string.
+            String serverResponseWithPortNumber = connectionIn.readLine();
+            // communicationPort is the unique communication port number as a Integer.
+            int communicationPort = Integer.parseInt(serverResponseWithPortNumber);
+            System.out.println("Communicating through PORT no: " + communicationPort);
+            // closing the connectionSocket as it is not needed anymore
+            connectionSocket.close();
+            // communicationSocket is making a connection request to the server using the
+            // serversIp and the new communicationPort received.
+            Socket communicationSocket = new Socket(SERVERIP, communicationPort);
+            // bufferedReader communicationIn is used to receive information from server.
+            BufferedReader communicationIn = new BufferedReader(new InputStreamReader(communicationSocket.getInputStream()));
+            // TEMPORARY BufferedReader fromUser will get an input from the user.
             BufferedReader fromUser = new BufferedReader(new InputStreamReader(System.in));
-            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+            // PrintWriter communicationOut is used to send messages to the server.
+            PrintWriter communicationOut = new PrintWriter(communicationSocket.getOutputStream(), true);
             System.out.println("Type name to see a random name. Or Quit.");
+            // continous loop
             while (true) {
-                String userIn = fromUser.readLine();
-                if (userIn.equals("quit")) {
+                // user input taken in.
+                String userInput = fromUser.readLine();
+                // break condition.
+                if (userInput.equals("quit")) {
                     break;
                 }
-                out.println(userIn);
-
-                String serverResponse = in.readLine();
-
-                System.out.println(serverResponse);
+                // sending the request to the server.
+                communicationOut.println(userInput);
+                // receiving the reply from the server.
+                String serverResponseCommunication = communicationIn.readLine();
+                System.out.println("The server returned: " + serverResponseCommunication);
             }
-            s.close();
+            // closing the communication socket.
+            communicationSocket.close();
+
         } catch (Exception e) {
             System.out.println(e);
         }
--- NetworkingAss/src/app/MyClientHandler.java
+++ NetworkingAss/src/app/MyClientHandler.java
@@ -1,38 +1,62 @@
 package app;
 
 import java.io.*;
-import java.net.Socket;
+import java.net.*;
+import java.util.*;
 
 public class MyClientHandler implements Runnable {
     private Socket myClient;
-    private PrintWriter out;
-    private BufferedReader in;
-    private static final int PORT = 6666;
+    private PrintWriter connectionOut;
+    private PrintWriter communicationOut;
+    private BufferedReader communicationIn;
 
+    // constructor
     public MyClientHandler(Socket myClientSocket) throws IOException {
+        // assignes the communication socket to myClient.
         this.myClient = myClientSocket;
-        in = new BufferedReader(new InputStreamReader(myClient.getInputStream()));
-        out = new PrintWriter(myClient.getOutputStream(), true);
-
+        // connectionOut is a printWriter that will be use to send the client a new PORT
+        // number.
+        connectionOut = new PrintWriter(myClient.getOutputStream(), true);
     }
 
     @Override
     public void run() {
         try {
+            // communicationPort is the unique port that the server and client will
+            // communicate through.
+            int communicationPort = MyServer.getComPort();
+            // the printerWriter connectionOut is used to send the client its unique
+            // communicationPort number.
+            connectionOut.println(communicationPort);
+            // establishess a new connection with the client using its unique PORT.
+            ServerSocket communicationServerSocket = new ServerSocket(communicationPort);
+            // communicationSocket waits for any response from the client.
+            Socket communicationSocket = communicationServerSocket.accept();
+            // communicationIn is a bufferedReader used to receive any information sent by
+            // the client on the unique Port.
+            communicationIn = new BufferedReader(new InputStreamReader(communicationSocket.getInputStream()));
+            // communicationOut is a printWriter used to send any information to the client
+            // through the unique port.
+            communicationOut = new PrintWriter(communicationSocket.getOutputStream(), true);
+            // continous loop
             while (true) {
-                String request = in.readLine();
+                // reads what the client sent.
+                String request = communicationIn.readLine();
+                // compare statement of what the client entered
                 if (request.contains("name")) {
-                    out.println(MyServer.getRandom());
+                    // TEMPORARY-- gets random name.
+                    communicationOut.println(MyServer.getRandom());
                 } else {
-                    out.println("Error. Type name to print out a random name. Or quit to quit the app.");
+                    communicationOut.println("Error. Type name to print out a random name. Or quit to quit the app.");
                 }
             }
         } catch (IOException e) {
 
         } finally {
             try {
-                in.close();
-                out.close();
+                // closing bufferedReader and printWriter
+                communicationIn.close();
+                communicationOut.close();
             } catch (IOException e) {
             }
         }

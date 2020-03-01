ae162b70f031b42f0baf331559568cb04c182af2
Silas <GRYSIL001@myuct.ac.za>
2020-03-01 18:42:05 +0200

    Working Upload and Download

--- NetworkingAss/src/app/MyClient.java
+++ NetworkingAss/src/app/MyClient.java
@@ -4,13 +4,13 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-
 public class MyClient {
 
     // Setting the constant connection port
     private static final int SERVERPORT = 6666;
     // setting the constant ip of the server
     private static final String SERVERIP = "localhost";
+     
 
     public static void main(String[] args) {
         try {
@@ -31,54 +31,196 @@ public class MyClient {
             // serversIp and the new communicationPort received.
             // Socket communicationSocket = new Socket(SERVERIP, communicationPort);
             // bufferedReader communicationIn is used to receive information from server.
-            BufferedReader communicationIn = new BufferedReader(
-                    new InputStreamReader(connectionSocket.getInputStream()));
+            BufferedReader communicationIn;
+            BufferedReader fromUser;
+            PrintWriter communicationOut;
+            OutputStream os = connectionSocket.getOutputStream();
 
             // TEMPORARY BufferedReader fromUser will get an input from the user.
-            BufferedReader fromUser = new BufferedReader(new InputStreamReader(System.in));
             // PrintWriter communicationOut is used to send messages to the server.
-            PrintWriter communicationOut = new PrintWriter(connectionSocket.getOutputStream(), true);
-            //System.out.println(menu);
-            
-            
+            // System.out.println(menu);
 
             // continous loop
-            String userInput="";
+            String userInput = "";
             Boolean flag = true;
             while (flag) {
+
+                communicationIn = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                fromUser = new BufferedReader(new InputStreamReader(System.in));
+                communicationOut = new PrintWriter(connectionSocket.getOutputStream(), true);
+
                 // user input taken in.
                 String cLine = communicationIn.readLine();
                 System.out.println(cLine);
+
                 while (!(cLine.equals(""))) {
-                
-                    //System.out.println(cLine != null);
-                    
 
                     cLine = communicationIn.readLine();
                     System.out.println(cLine);
-                    
-                    //System.out.println(menu);
-                    
-                    
+
                 }
+
+                // Taking user input
                 userInput = fromUser.readLine();
-                
-                
+
                 // sending the request to the server.
                 communicationOut.println(userInput);
-                // receiving the reply from the server.
-                if((userInput.equals("4"))||(userInput.equals("QUIT"))){
-                    System.out.println(communicationIn.readLine());
-                    flag=false;
+
+                switch (userInput) {
+                    case "1":
+                    case "UPLOAD":
+                        upload(communicationIn, communicationOut, fromUser, os);
+                        break;
+
+                    case "2":
+                    case "DOWNLOAD":
+                        download(communicationIn, communicationOut, fromUser, connectionSocket);
+                        break;
+
+                    case "3":
+                    case "QUERY":
+                        // do something
+                        break;
+
+                    case "4":
+                    case "QUIT":
+                        System.out.println(communicationIn.readLine());
+                        flag = false;
+                        break;
+
                 }
-                
+
             }
-            
+
             // closing the communication socket.
             connectionSocket.close();
 
         } catch (Exception e) {
-            System.out.println(e);
+            // System.out.println(e);
+        }
+    }
+
+    private static void upload(BufferedReader communicationIn, PrintWriter communicationOut, BufferedReader fromUser,
+            OutputStream os) {
+
+        try {
+            //Ask for upload file on the client side
+            System.out.println("Enter the filename");
+            String filename = fromUser.readLine();
+
+            //Find file in the users loal directory
+            String directory_path = System.getProperty("user.dir") + "/NetworkingAss/";
+            File transferFile = new File(directory_path + filename);
+
+            //File does not exist
+            if(!transferFile.exists())
+            {
+                //Display message to the user
+                System.out.println("Error 404");
+
+                //Notify message to the server
+                communicationOut.println("Error 404");
+            }
+
+            //File exists
+            //Upload toserver
+            else
+            {
+                //Send filename to the server
+                communicationOut.println(filename);
+                //Sedn file length to the server
+                communicationOut.println(transferFile.length());
+                
+                //Transform file data into bytearray
+                byte[] bytearray = new byte[(int) transferFile.length()];
+                FileInputStream fin = new FileInputStream(transferFile);
+                BufferedInputStream bin = new BufferedInputStream(fin);
+                bin.read(bytearray, 0, bytearray.length);
+                System.out.println("Sending Files...");
+                
+                //Write results to the server
+                os.write(bytearray, 0, bytearray.length);
+                os.flush();
+                
+                //Requires acknowledgment of client to synchronize their processes
+                communicationIn.readLine();
+                System.out.println("done");
+
+                //Starts to send the file content
+
+            }
+
+
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void download(BufferedReader communicationIn, PrintWriter communicationOut, BufferedReader fromUser,
+            Socket connectionSocket) {
+
+        try {
+
+            // Read the servers filename request and print to user
+            String service_file_request = communicationIn.readLine();
+            System.out.print(service_file_request);
+
+            // Record the user's requested file and send to server
+            String filename = fromUser.readLine();
+            communicationOut.println(filename);
+
+            // Download file or Display 404 message
+            String server_header = communicationIn.readLine();
+
+            // Not found action
+            if (server_header.equalsIgnoreCase("Error 404")) {
+                System.out.println("Error 404");
+
+            }
+            // Download incoming file
+            else {
+
+                // Setup to read the bytestream
+                int filesize = Integer.parseInt(communicationIn.readLine());
+                int bytesRead;
+                int currentTot = 0;
+                byte[] bytearray = new byte[filesize];
+
+                // Variables for server input and stream objects that write to a local file
+                InputStream is = connectionSocket.getInputStream();
+                FileOutputStream fos = new FileOutputStream("copy.txt");
+                BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+                // Read the first byte stream
+                bytesRead = is.read(bytearray, 0, bytearray.length);
+                currentTot = bytesRead;
+
+                // Read the server's remaining byte streams in chunks
+                do {
+                    // Reads the bytestream and adds it to the byteArray
+                    bytesRead = is.read(bytearray, currentTot, (bytearray.length - currentTot));
+                    // Updates the remaining bytes to be read
+                    if (bytesRead > 0)
+                        currentTot += bytesRead;
+                } while (bytesRead > 0);
+
+                // Write the locally stored byte streams into client file
+                bos.write(bytearray, 0, currentTot);
+                bos.flush();
+
+                // Close the filewriting outputstream object
+                bos.close();
+
+                // Send acknowledgement to server to synchronize their progress
+                communicationOut.println("Client has downloaded file");
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 }
--- NetworkingAss/src/app/MyClientHandler.java
+++ NetworkingAss/src/app/MyClientHandler.java
@@ -9,6 +9,7 @@ public class MyClientHandler implements Runnable {
     private PrintWriter connectionOut;
     private PrintWriter communicationOut;
     private BufferedReader communicationIn;
+    private OutputStream os;
 
     // constructor
     public MyClientHandler(Socket myClientSocket) throws IOException {
@@ -16,7 +17,7 @@ public class MyClientHandler implements Runnable {
         this.myClient = myClientSocket;
         // connectionOut is a printWriter that will be use to send the client a new PORT
         // number.
-        connectionOut = new PrintWriter(myClient.getOutputStream(), true);
+
     }
 
     @Override
@@ -24,16 +25,18 @@ public class MyClientHandler implements Runnable {
         try {
             // communicationPort is the unique port that the server and client will
             // communicate through.
-           // int communicationPort = MyServer.getComPort();
+            // int communicationPort = MyServer.getComPort();
             // the printerWriter connectionOut is used to send the client its unique
             // communicationPort number.
-           // connectionOut.println(communicationPort);
+            // connectionOut.println(communicationPort);
             // establishess a new connection with the client using its unique PORT.
-           // ServerSocket communicationServerSocket = new ServerSocket(communicationPort);
+            // ServerSocket communicationServerSocket = new ServerSocket(communicationPort);
             // communicationSocket waits for any response from the client.
-          //  Socket communicationSocket = communicationServerSocket.accept();
+            // Socket communicationSocket = communicationServerSocket.accept();
             // communicationIn is a bufferedReader used to receive any information sent by
             // the client on the unique Port.
+            os = myClient.getOutputStream();
+            connectionOut = new PrintWriter(os, true);
 
             communicationIn = new BufferedReader(new InputStreamReader(myClient.getInputStream()));
             // communicationOut is a printWriter used to send any information to the client
@@ -43,19 +46,21 @@ public class MyClientHandler implements Runnable {
             boolean flag = true;
             while (flag) {
                 // reads what the client sent.
-                    String  menu="1) UPLOAD \n2) DOWNLOAD \n3) QUERY \n4) QUIT \n";
-                    communicationOut.println(menu);
+                System.out.println("Top of the menu");
+                String menu = "1) UPLOAD \n2) DOWNLOAD \n3) QUERY \n4) QUIT \n";
+                communicationOut.println(menu);
                 String request = communicationIn.readLine();
 
                 // compare statement of what the client entered
                 switch (request) {
                     case "1":
                     case "UPLOAD":
-                        System.out.println("Told me to upload");
+                        upload_query();
                         break;
                     case "2":
                     case "DOWNLOAD":
-                        System.out.println("Told me to download");
+                        download_query();
+                        System.out.println("server switch after download");
                         break;
                     case "3":
                     case "QUERY":
@@ -64,8 +69,8 @@ public class MyClientHandler implements Runnable {
                     case "4":
                     case "QUIT":
                         System.out.println("Told me to QUIT");
-                        communicationOut.println("connection on port #"+myClient.getPort()+" is closed");
-                        flag=false;
+                        communicationOut.println("connection on port #" + myClient.getPort() + " is closed");
+                        flag = false;
                         break;
                     default:
                         break;
@@ -79,10 +84,125 @@ public class MyClientHandler implements Runnable {
                 // closing bufferedReader and printWriter
                 communicationIn.close();
                 communicationOut.close();
+                os.close();
                 System.out.println("Closed stuff");
             } catch (IOException e) {
             }
         }
 
     }
+
+    private void upload_query() {
+
+        
+        try {
+            //Determines whether user entered existent file
+            String client_file_result = communicationIn.readLine();
+            //Execute upload if the file is valid
+            if (!client_file_result.equalsIgnoreCase("Error 404"))
+            {
+                //Name of the file to be uploaded
+                String filename = client_file_result;
+                
+                 // Setup to read the bytestream
+                 int filesize = Integer.parseInt(communicationIn.readLine());
+                 int bytesRead;
+                 int currentTot = 0;
+                 byte[] bytearray = new byte[filesize];
+ 
+                 // Variables for server input and stream objects that write to a local file
+                 InputStream is = myClient.getInputStream();
+                 FileOutputStream fos = new FileOutputStream("Server_Copy_Of_"+filename);
+                 BufferedOutputStream bos = new BufferedOutputStream(fos);
+ 
+                 // Read the first byte stream
+                 bytesRead = is.read(bytearray, 0, bytearray.length);
+                 currentTot = bytesRead;
+ 
+                 // Read the server's remaining byte streams in chunks
+                 do {
+                     // Reads the bytestream and adds it to the byteArray
+                     bytesRead = is.read(bytearray, currentTot, (bytearray.length - currentTot));
+                     // Updates the remaining bytes to be read
+                     if (bytesRead > 0)
+                         currentTot += bytesRead;
+                 } while (bytesRead > 0);
+ 
+                 // Write the locally stored byte streams into client file
+                 bos.write(bytearray, 0, currentTot);
+                 bos.flush();
+ 
+                 // Close the filewriting outputstream object
+                 bos.close();
+ 
+                 // Send acknowledgement to server to synchronize their progress
+                 communicationOut.println("Server has downloaded file");                
+                
+
+
+            }
+
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+
+
+    }
+
+    private void download_query() {
+
+        // Ask for file to be downloaded
+        communicationOut.println("Enter the filename: ");
+        try {
+            // Receive filename and send it to user or error 404 if not found
+            String filename = communicationIn.readLine();
+
+            // Retrieve requested_file in servers system
+            System.out.println("The servers directory path is " + System.getProperty("user.dir"));
+            String directory_path = System.getProperty("user.dir") + "/NetworkingAss/";
+            File transferFile = new File(directory_path + filename);
+
+            // Send file or not found response
+
+            // Does not exist
+            if (!transferFile.exists()) {
+                communicationOut.println("Error 404");
+            }
+            // File exists
+            // Send back to user
+            else {
+                
+                communicationOut.println("Ok(200)");
+
+                // Sending file to client
+                communicationOut.println(transferFile.length());
+                byte[] bytearray = new byte[(int) transferFile.length()];
+                FileInputStream fin = new FileInputStream(transferFile);
+                BufferedInputStream bin = new BufferedInputStream(fin);
+                bin.read(bytearray, 0, bytearray.length);
+                System.out.println("Sending Files...");
+                os.write(bytearray, 0, bytearray.length);
+                os.flush();
+                //os.close();
+                
+                //Requires acknowledgment of client to synchronize their processes
+                communicationIn.readLine();
+                System.out.println("done");
+
+                //Acknowledgment of user
+                
+            }
+
+        }
+
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
 }
